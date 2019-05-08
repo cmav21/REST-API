3b4f8352ddd926e253c42733f9d3932c88c130c1
cesar magallon <cesar9mv@gmail.com>
2019-05-08 18:39:03 -0500

    [add]cadena de conexion

--- src/database.js
+++ src/database.js
@@ -1,3 +1,16 @@
 import MongoClient from "mongodb";
 
-MongoClient.connect();
+//Regresa cadena de conexion y exporta la funcion
+export async function connect(){
+    try{
+        //Si la base de datos no existe la crea
+        const client = await MongoClient.connect('mongodb://localhost:27017', {
+            useNewUrlParser: true
+        });
+        const db = client.db('node-restapi');
+        console.log("DB is connected");
+        return db;
+    } catch(e){
+        console.log(e);
+    }
+}
