075ed556446ad55b257898b309022dcedf292c24
cesar magallon <cesar9mv@gmail.com>
2019-05-08 18:59:00 -0500

    [add]export taks routes

--- src/server.js
+++ src/server.js
@@ -1,12 +1,18 @@
+const app = express();
+
+//Routes
 import express from 'express';
 import router from "./routes/index.routes";
-
-const app = express();
+import TaskRoutes from "./routes/task.routes"
 
 //Settings
 app.set('port', process.env.PORT || 3000);
 
+//Middleware, funciones que se ejecutan antes de ejecutar las rutas
+
+
 //Routes
 app.use(router);
+app.use('/tasks',TaskRoutes);
 
 export default app; 
