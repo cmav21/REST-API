5f6ec2bf134b4757b32d29c0c8f8f0eda66e5fb9
cesar magallon <cesar9mv@gmail.com>
2019-05-08 19:50:45 -0500

    [add]compile src with babel

--- dist/server.js
+++ dist/server.js
@@ -7,14 +7,23 @@ exports["default"] = void 0;
 
 var _express = _interopRequireDefault(require("express"));
 
+var _index = _interopRequireDefault(require("./routes/index.routes"));
+
+var _task = _interopRequireDefault(require("./routes/task.routes"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-var app = (0, _express["default"])(); //Settings
+// import express, {json} from 'express';
+//Another way to import json
+var app = (0, _express["default"])(); //Routes
 
-app.set('port', process.env.PORT || 3000); //Routes
+//Settings
+app.set('port', process.env.PORT || 3000); //Middleware, functions which are executed before access to routes
 
-app.get('/', function (req, res) {
-  return res.send('Hello world');
-});
+app.use(_express["default"].json()); //This to understand the json format
+//Routes
+
+app.use(_index["default"]);
+app.use('/tasks', _task["default"]);
 var _default = app;
 exports["default"] = _default;
